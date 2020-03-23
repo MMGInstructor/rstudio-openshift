16316493b61c06f0c0fea9f775e23fd011894d8a
MartaMG <59799120+MMGInstructor@users.noreply.github.com>
2020-03-23 13:38:45 +0100

    Update fix.sh

--- server/fix.sh
+++ server/fix.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 echo "Creating passwd entries"
-tempfile=$(mktemp)
-sed -e "s/^rstudio.*//g" /etc/passwd > $tempfile
-sed -e "s/^$USERNAME.*//g" /etc/passwd > $tempfile
-#Eliminio lineas vacias
-sed -e "/^$/d" $tempfile
+tempfile=$(mktemp)  
+#Eliminio lineas de usuarios rstudio y $USERNAME y lineas vacias
+sed -e "s/^rstudio.*//g" /etc/passwd | sed -e "s/^$USERNAME.*//g" | sed "/^$/d" > $tempfile
 cp $tempfile /etc/passwd
+
 rm -f $tempfile
 echo "rstudio-server:x:$(id -u):$(id -g)::$HOME:" >> /etc/passwd
 echo "$USERNAME:x:$(id -u):$(id -g)::$HOME:" >> /etc/passwd
 
 echo "Creating group entries"
 tempfile2=$(mktemp)
-sed -e "s/^rstudio.*//g" /etc/group > $tempfile2
-sed -e "s/^$USERNAME.*//g" /etc/group > $tempfile2
+# Elimino grupo rstudio y $USERNAME y lineas vacías
+sed -e "s/^rstudio.*//g" /etc/group | sed -e "s/^$USERNAME.*//g" | sed "/^$/d" > $tempfile2
 cp $tempfile2 /etc/group
+
 rm -f $tempfile2
-echo "$USERNAME:x:$(id -u):" >> /etc/group
+#echo "$USERNAME:x:$(id -u):" >> /etc/group
 
 echo "Reading credentials"
 RPassword=$(cat /tmp/shadow/shadow.crypt)
 
 echo "Creating shadow entries"
-tempfile3=$(mktemp)
-sed -e "s/^rstudio.*//g" /etc/shadow > $tempfile3
-sed -e "s/^$USERNAME.*//g" /etc/shadow > $tempfile3
+tempfile3=$(mktemp)   
+# Elimino passwords de rstudio y USERNAME y lineas vacias
+sed -e "s/^rstudio.*//g" /etc/shadow | sed -e "s/^$USERNAME.*//g" | sed "/^$/d" > $tempfile3
+
 cp $tempfile3 /etc/shadow
 rm -f $tempfile3
 echo "$USERNAME:${RPassword}:17866:0:99999:7:::" >> /etc/shadow
